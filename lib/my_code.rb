f288862c2bfe9c2e20af917a82e08238a6230c7b
Carl Parm <carlparm@gmail.com>
2020-01-23 16:12:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -58,7 +58,7 @@ end
 def reduce_to_any_true(source_array)
   index = 0
   while index < source_array.length do
-    if source_array[index] != true
+   if source_array.include? false
       return false
     else
       return true
